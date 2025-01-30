833fb43961d06e61ad197737c79ec03ed2329c92
lvbealr <lvbealr@mail.ru>
2025-01-31 02:15:38 +0300

    static struct to save options values, universal func, readme

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # simpleConsoleParser
+
+## Usage
+#### `consoleParser.h`
+```c
+// INSERT OPTIONS CREATING HERE. Ex.:
+#define INITIALIZE_OPTION_LIST() {
+    CREATE_OPTION(options, "dumpFolder", 2, 'd', 0); \
+}
+...
+// CREATE STRUCTURE FIELDS IF NECESSARY TO RECORD DATA. Ex.:
+struct _OPTIONS_ {
+    const char *dumpFile = {};
+};
+...
+
+// SET YOUR OWN VALUE OF MAX_OPTIONS_COUNT!
+static const size_t MAX_OPTIONS_COUNT  = 1;
+// CREATE NECESSARY OBJECTS! Ex.:
+static const size_t MAX_DUMP_FILE_NAME = 50;
+...
+
+// INITIALIZE STRUCTURE FIELDS! Ex.:
+parseError __OPTIONS_DATA_INITIALIZE__() {
+    __OPTIONS_DATA__.dumpFile = (char *)calloc(MAX_DUMP_FILE_NAME, sizeof(char));
+    customWarning(__OPTIONS_DATA__.dumpFile != NULL, parseError::CALLOC_ERROR);
+    
+    return parseError::NO_ERRORS;
+}
+
+// CLEAR THE FIELD OF THE STRUCTURE, FREE VIRTUAL MEMORY IF NECESSARY! Ex.:
+parseError __OPTIONS_DATA_DESTRUCT__() {
+//    FREE_(__OPTIONS_DATA__.dumpFile);
+    return parseError::NO_ERRORS;
+}
+
+parseError parseConsole(int argc, char *argv[]) {
+    ...
+    // SET YOUR OWN VALUE OF optString! (use bash >> man getopt). Ex.:
+    const char *optString = "d:";
+    ...
+    
+    while ((gotOption = getopt_long(argc, argv, optString, options, NULL))
+            != parseError::NO_MORE_OPTIONS) {
+        ...
+        // COME UP WITH YOUR OWN INSTRUCTIONS FOR VARIOUS FLAGS
+        // (IMPORTANT! WRITE DATA TO THE __OPTIONS_DATA__ STRUCTURE). Ex.:
+        switch (gotOption) {
+             case 'd': {
+                 strncpy((char *)__OPTIONS_DATA__.dumpFile, optarg, MAX_DUMP_FILE_NAME);
+                 break;
+             }
+             ...
+             default:
+                 break;
+        }
+    }
+    
+    return parseError::NO_ERRORS;
+}
+
+#endif // CONSOLE_PARSER_H_
+```
+
+#### `main.cpp`
+```c
+int main(int argc, char *argv[]) {
+    __OPTIONS_DATA_INITIALIZE__();
+    parseConsole(argc, argv);
+    __OPTIONS_DATA_DESTRUCT__();
+    ...
+}
+```
--- consoleParser.h
+++ consoleParser.h
@@ -1,40 +1,81 @@
+/* ############################################################################################################### */
+/* Do not forget to INITIALIZE and DESTROY the __OPTIONS_DATA__ structure after use in main() (or where necessary) */
+/* ############################################################################################################### */
+
 #ifndef CONSOLE_PARSER_H_
 #define CONSOLE_PARSER_H_
 
 #include <cstdlib>
+#include <stdio.h>
 #include <getopt.h>
+#include <string.h>
 
-#include <consoleParser.h>
 #include "customWarning.h"
 
-#define INIT_OPTION(optionsBuffer, flagName, hasArgValue, shortName, flagValue) { \
-  customWarning(optionsCount < MAX_OPTIONS_COUNT, (void) 1);                      \
-                                                                                  \
-  optionsBuffer[optionsCount].name    = flagName;                                 \
-  optionsBuffer[optionsCount].has_arg = hasArgValue;                              \
-  optionsBuffer[optionsCount].val     = shortName;                                \
-  optionsBuffer[optionsCount].flag    = flagValue;                                \
-                                                                                  \
-  optionsCount++;                                                                 \
+#define FREE_(ptr) { \
+    free(ptr);       \
+    ptr = NULL;      \
 }
 
+#define CREATE_OPTION(optionsBuffer, flagName, hasArgValue, shortName, flagValue) { \
+  customWarning(optionsCount < MAX_OPTIONS_COUNT, (void) 1);                        \
+                                                                                    \
+  optionsBuffer[optionsCount].name    = flagName;                                   \
+  optionsBuffer[optionsCount].has_arg = hasArgValue;                                \
+  optionsBuffer[optionsCount].val     = shortName;                                  \
+  optionsBuffer[optionsCount].flag    = flagValue;                                  \
+                                                                                    \
+  optionsCount++;                                                                   \
+}                                                                                   \
+
+#define INITIALIZE_OPTION_LIST() {}
+
 #ifndef _NDEBUG
-  #define PRINT_OPTION() printf("Option: [-%c]\t %s\n", gotOption, optarg);
+  #define PRINT_OPTION() printf("Option: [-%c]\t %s\n", gotOption, optarg)
+#else
+    #define PRINT_OPTION()
 #endif // _NDEBUG
 
-static const size_t MAX_OPTIONS_COUNT = 2;
+struct _OPTIONS_ {};
 
-void parseConsole(int argc, char *argv[]) {
-  size_t optionsCount = 0;
-  option options[MAX_OPTIONS_COUNT]   = {};
+enum class parseError {
+    NO_MORE_OPTIONS = -1,
+    NO_ERRORS       =  0,
+    CALLOC_ERROR    =  1
+};
 
-  INIT_OPTION(options, "dumpFolder", 2, 'd', 0);
-  INIT_OPTION(options, "dataBase",   2, 'b', 0);
+static const size_t MAX_OPTIONS_COUNT  = 1;
 
-  int   gotOption    = 0;
-  while ((gotOption = getopt_long(argc, argv, "2", options, NULL)) != -1) {
-    PRINT_OPTION();
-  }
+static _OPTIONS_ __OPTIONS_DATA__ = {};
+
+parseError __OPTIONS_DATA_INITIALIZE__() {
+    return parseError::NO_ERRORS;
+}
+
+parseError __OPTIONS_DATA_DESTRUCT__() {
+    return parseError::NO_ERRORS;
+}
+
+parseError parseConsole(int argc, char *argv[]) {
+    size_t optionsCount                 = 0;
+    option options[MAX_OPTIONS_COUNT]   = {};
+    const char *optString               = "";
+
+    INITIALIZE_OPTION_LIST();
+
+    int gotOption = 0;
+
+    while ((gotOption = getopt_long(argc, argv, optString, options, NULL))
+            != (int) parseError::NO_MORE_OPTIONS) {
+        PRINT_OPTION();
+
+        switch (gotOption) {
+            default:
+                break;
+        }
+    }
+
+    return parseError::NO_ERRORS;
 }
 
 #endif // CONSOLE_PARSER_H_
